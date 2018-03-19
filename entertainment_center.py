e482e7d2e064f11e7d21cb6912884878bf761977
Subhadeep Dey <contact.sdey@gmail.com>
2018-03-19 22:51:58 +0530

    Code optimizations.

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,13 +5,13 @@ import fresh_tomatoes
 # Create instances of class `Movie` - for each movie.
 
 the_terminator = media.Movie("The Terminator",
-                            "1 hour 48 minutes",
-                            "The story of a cyborg sent past in time.", 
-                            "Sci-fi, Action",
-                            "26 October 1984",
-                            "8.0",
-                            "https://goo.gl/J8m6ww",
-                            "https://youtu.be/c4Jo8QoOTQ4")
+                             "1 hour 48 minutes",
+                             "The story of a cyborg sent past in time.",
+                             "Sci-fi, Action",
+                             "26 October 1984",
+                             "8.0",
+                             "https://goo.gl/J8m6ww",
+                             "https://youtu.be/c4Jo8QoOTQ4")
 
 avatar = media.Movie("Avatar",
                      "2 hours 42 minutes",
@@ -19,27 +19,27 @@ avatar = media.Movie("Avatar",
                      "Action, Adventure, Sci-fi, Fantasy",
                      "18 December 2009",
                      "7.8",
-                     "https://res.cloudinary.com/sdey96/image/upload/v1521396344/MPW-50968_ghubcf.jpg",
+                     "https://goo.gl/eTxWYy",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 
-avengers3 = media.Movie("Avengers: Infinity War", 
+avengers3 = media.Movie("Avengers: Infinity War",
                         "2 hours 36 minutes",
-                        "Avengers assemble to defeat Thanos.", 
+                        "Avengers assemble to defeat Thanos.",
                         "Sci-fi, Action",
                         "25 April 2018",
                         "8.2",
                         "https://goo.gl/YSEU5c",
                         "https://youtu.be/6ZfuNTqbHE8")
 
-maze_runner2= media.Movie("Maze Runner 2",
-                          "2 hours 13 minutes",
-                          "Few selected ones save themselves from an organisation called WCKD.", 
-                          "Sci-Fi, Dystopian, Adventure",  
-                          "18 September 2015",
-                          "6.3",
-                          "https://goo.gl/8XtvMQ", 
-                          "https://youtu.be/SDofO3P2HpE")
+maze_runner2 = media.Movie("Maze Runner 2",
+                           "2 hours 13 minutes",
+                           "Few selected ones save themselves from an organisation called WCKD.",
+                           "Sci-Fi, Dystopian, Adventure",
+                           "18 September 2015",
+                           "6.3",
+                           "https://goo.gl/8XtvMQ",
+                           "https://youtu.be/SDofO3P2HpE")
 
 thor_ragnarok = media.Movie("Thor Ragnarok",
                             "2 hours 10 minutes",
